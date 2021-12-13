7958df1553a6c93f0a570922f78040c6ea14b9f3
YingMuo <wl03452329@gmail.com>
2021-12-13 16:04:26 +0800

    Finish stage 1

--- src/main.asm
+++ src/main.asm
@@ -12,6 +12,13 @@ extern strncmp
 extern strtok
 extern strchr
 extern strcpy
+extern atoi
+extern fopen
+extern fclose
+extern rewind
+extern fprintf
+extern fscanf
+
 
 section .data
 
@@ -31,30 +38,83 @@ str_sym:
     db "sym", 0
 str_err:
     db "err: %d", 10, 0
+mode_r:
+    db "r", 0
+mode_w:
+    db "w", 0
+
+
+section .bss
+input_file:
+    resq 1
+output_file:
+    resq 1
+addrcnt:
+    resq 1
 
 
 section .text
 
 main:
+    %define argv     qword [rbp - 0x10]
+    %define argc     qword [rbp - 0x8]
+
+    push rbp
+    mov rbp, rsp
+    sub rsp, 0x20
+    mov argc, rdi
+    mov argv, rsi
+
+    mov rax, argv
+    mov rdi, qword [rax + 8 * 1]
+    mov rsi, mode_r
+    call fopen
+    mov qword [input_file], rax
+
+    ; mov rax, argv
+    ; mov rdi, qword [rax + 8 * 2]
+    ; mov rsi, mode_w
+    ; call fopen
+    ; mov qword [output_file], rax
+
+    call stg1
+
+    mov rdi, qword [input_file]
+    call rewind
+
+    mov rdi, qword [input_file]
+    call fclose
+
+    ; mov rdi, qword [output_file]
+    ; call fclose
+
+    leave
+    ret
+
+stg1:
     %define idx     qword [rbp - 0x8]
     %define tok     qword [rbp - 0x10]
-    %define addrcnt qword [rbp - 0x18]
-    %define inc_id  qword [rbp - 0x20]
+    %define inc_id  qword [rbp - 0x18]
     %define buf     [rbp - 0x60]
 
     push rbp
     mov rbp, rsp
     sub rsp, 0x60
     mov qword [symcnt], 0
-    mov addrcnt, 0
-
-.loop:
-    mov rdi, input_str
-    call printf wrt ..plt
+    mov qword [addrcnt], 0
 
     lea rdi, buf
     mov rsi, 0x50
-    mov rdx, qword [stdin]
+    mov rdx, qword [input_file]
+    call fgets wrt ..plt
+
+.loop:
+    ; mov rdi, input_str
+    ; call printf wrt ..plt
+
+    lea rdi, buf
+    mov rsi, 0x50
+    mov rdx, qword [input_file]
     call fgets wrt ..plt
 
     lea rdi, buf
@@ -63,8 +123,8 @@ main:
     lea rdi, buf
     mov byte [rdi + rax], 0
 
-    lea rdi, buf
-    call puts wrt ..plt
+    ; lea rdi, buf
+    ; call puts wrt ..plt
 
     lea rdi, buf
     mov rsi, delim
@@ -76,7 +136,7 @@ main:
     je .over_sym
 
     mov rdi, tok
-    mov rsi, addrcnt
+    mov rsi, qword [addrcnt]
     call add_sym
 
     call print_sym
@@ -102,11 +162,55 @@ main:
     ; mov rdi, fmt_str2
     ; call printf
 
-    mov rdi, inc_id
-    call get_inc_len
-    add addrcnt, rax
+    mov rax, inc_id
+    cmp rax, 0x5
+    je .leave
+.addbyte:
+    mov rax, inc_id
+    test rax, rax
+    jne .addword
+    add qword [addrcnt], 0x1
+    jmp .loop
 
-    ; mov rsi, addrcnt
+.addword:
+    mov rax, inc_id
+    cmp rax, 0x1
+    jne .addnbyte
+    add qword [addrcnt], 0x3
+    jmp .loop
+
+.addnbyte:
+    mov rax, inc_id
+    cmp rax, 0x2
+    jne .addnword
+    xor rdi, rdi
+    mov rsi, delim
+    call strtok
+    mov rdi, rax
+    call atoi
+    add qword [addrcnt], rax
+    jmp .loop
+    
+.addnword:
+    mov rax, inc_id
+    cmp rax, 0x3
+    jne .addlen
+    xor rdi, rdi
+    mov rsi, delim
+    call strtok
+    mov rdi, rax
+    call atoi
+    mov rcx, 3
+    mul rcx
+    add qword [addrcnt], rax
+    jmp .loop
+
+.addlen:
+    mov rdi, inc_id
+    call get_ins_len
+    add qword [addrcnt], rax
+
+    ; mov rsi, qword [addrcnt]
     ; mov rdi, fmt_str2
     ; call printf
 
@@ -120,3 +224,5 @@ main:
     mov rdi, str_err
     call printf
     jmp .leave
+
+stg2:
